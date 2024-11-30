9358edf720d36ff71c7f3fc69a9f0e388c56d858
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-11-30 21:10:40 +0530

    basic implementation

--- authentication-service/app.js
+++ authentication-service/app.js
@@ -2,6 +2,10 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const authRouter = require("./routes/authRoutes");
+
 /// Create an instance of Express
 const app = express();
 
@@ -16,5 +20,14 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
+
+/// 2). ROUTES
+app.use("/api/v1", authRouter);
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+/// 3). ERROR HANDLER MIDDLEWARE
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ authentication-service/controllers/authController.js
@@ -0,0 +1,11 @@
+exports.register = async (req, res) => {
+  res.status(404).json({
+    status: "not implemented",
+  });
+};
+
+exports.login = async (req, res) => {
+  res.status(404).json({
+    status: "not implemented",
+  });
+};

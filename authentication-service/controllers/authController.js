4a74f887b9ae4b3b828076308b2ddb40480672f8
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-01 20:21:32 +0530

    auth -> register endpoint

--- authentication-service/controllers/authController.js
+++ authentication-service/controllers/authController.js
@@ -1,13 +1,26 @@
+const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
-// const bcrypt = require('bcrypt');
+const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-const { userSchema } = require("../models/userModel");
-const { generateToken } = require("../utils/jwtUtils");
+const { signToken, verifyToken } = require("../utils/jwtUtils");
 
 exports.register = catchAsync(async (req, res, next) => {
-  res.status(404).json({
-    status: "not implemented",
+  const user = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
   });
 });
 
--- authentication-service/utils/jwtUtils.js
+++ authentication-service/utils/jwtUtils.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
@@ -11,6 +11,6 @@ const verifyToken = (token) => {
 };
 
 module.exports = {
-  generateToken,
+  signToken,
   verifyToken,
 };

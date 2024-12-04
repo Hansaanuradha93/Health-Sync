80766961e5beb89978596a7cb7ce42525f5e74ca
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-05 00:48:46 +0530

    email sending logic

--- notification-service/controllers/notificationController.js
+++ notification-service/controllers/notificationController.js
@@ -1,10 +1,26 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const sendEmail = require("../utils/emailService");
 
-/// Get all appointments
+/// Send email notification
 exports.sendNotification = catchAsync(async (req, res, next) => {
   console.log(req.body);
   res.status(200).json({
     status: "not implemented",
   });
 });
+
+const sendNotification = catchAsync(async (req, res, next) => {
+  const { to, subject, text } = req.body;
+
+  // Send email
+  await sendEmail(to, subject, text);
+
+  // Send response
+  res.status(200).json({
+    status: "success",
+    message: "Email sent successfully",
+  });
+});
+
+module.exports = { sendNotification };

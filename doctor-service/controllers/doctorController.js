b7ebe3e443627ed8084dbdbf134581e695fd0241
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-08 18:02:43 +0530

    endpoints implemented

--- doctor-service/controllers/doctorController.js
+++ doctor-service/controllers/doctorController.js
@@ -4,35 +4,66 @@ const AppError = require("../utils/appError");
 
 // Create a new doctor
 exports.createDoctor = catchAsync(async (req, res, next) => {
+  const doctor = await Doctor.create(req.body);
+
   res.status(201).json({
-    status: "not implemented",
+    status: "success",
+    data: { doctor },
   });
 });
 
 /// Get all doctors
 exports.getAllDoctors = catchAsync(async (req, res, next) => {
-  res.status(201).json({
-    status: "not implemented",
+  const doctors = await Doctor.find();
+
+  res.status(200).json({
+    status: "success",
+    results: doctors.length,
+    data: { doctors },
   });
 });
 
 /// Get a specific doctor
 exports.getDoctorById = catchAsync(async (req, res, next) => {
-  res.status(201).json({
-    status: "not implemented",
+  const doctor = await Doctor.findById(req.params.id);
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { doctor },
   });
 });
 
-/// Update an doctor
+/// Update a doctor
 exports.updateDoctor = catchAsync(async (req, res, next) => {
-  res.status(201).json({
-    status: "not implemented",
+  const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { doctor },
   });
 });
 
 /// Delete an doctor
 exports.deleteDoctor = catchAsync(async (req, res, next) => {
-  res.status(201).json({
-    status: "not implemented",
+  const doctor = await Doctor.findByIdAndDelete(req.params.id);
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
   });
 });

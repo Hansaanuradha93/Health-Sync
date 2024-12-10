76d0959e9cf25f6d3ce39ef47e44a1352934a5c6
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-10 11:38:31 +0530

    doctor service integrated

--- api-gateway/controllers/gatewayController.js
+++ api-gateway/controllers/gatewayController.js
@@ -3,6 +3,44 @@ const AppError = require("../utils/appError");
 const axios = require("axios");
 
 exports.connectPatientService = catchAsync(async (req, res, next) => {
+  const url = `${process.env.PATIENT_SERVICE_URL}${req.originalUrl}`;
+
+  if (req.method === "GET") {
+    const response = await axios.get(url);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "POST") {
+    const response = await axios.post(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method == "PATCH") {
+    const response = await axios.patch(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "DELETE") {
+    const response = await axios.delete(url);
+    res.status(response.status).json(response.data);
+  }
+});
+
+exports.connectAppointmentService = catchAsync(async (req, res, next) => {
+  const url = `${process.env.APPOINTMENT_SERVICE_URL}${req.originalUrl}`;
+
+  if (req.method === "GET") {
+    const response = await axios.get(url);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "POST") {
+    const response = await axios.post(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method == "PATCH") {
+    const response = await axios.patch(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "DELETE") {
+    const response = await axios.delete(url);
+    res.status(response.status).json(response.data);
+  }
+});
+
+exports.connectDoctorService = catchAsync(async (req, res, next) => {
+  const url = `${process.env.DOCTOR_SERVICE_URL}${req.originalUrl}`;
+
   if (req.method === "GET") {
     const response = await axios.get(url);
     res.status(response.status).json(response.data);
--- api-gateway/routes/gatewayRoutes.js
+++ api-gateway/routes/gatewayRoutes.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middleware/authenticate");
 
-const { connectPatientService } = require("../controllers/gatewayController");
+const {
+  connectPatientService,
+  connectAppointmentService,
+  connectDoctorService,
+} = require("../controllers/gatewayController");
 
 // Patient Routes
 router.use("/api/v1/patients", authenticate, connectPatientService);
+
+// Appointment Routes
+router.use("/api/v1/appointments", authenticate, connectAppointmentService);
+
+// Doctor Routes
+router.use("/api/v1/doctors", authenticate, connectDoctorService);
 
 module.exports = router;

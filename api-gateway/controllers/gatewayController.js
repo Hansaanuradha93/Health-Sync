3f50794400dc771de5ae4c81720626c30071331a
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-10 10:50:52 +0530

    patient service connected

--- api-gateway/controllers/gatewayController.js
+++ api-gateway/controllers/gatewayController.js
@@ -3,11 +3,17 @@ const AppError = require("../utils/appError");
 const axios = require("axios");
 
 exports.connectPatientService = catchAsync(async (req, res, next) => {
-  const response = await axios({
-    method: req.method,
-    url: `${process.env.PATIENT_SERVICE_URL}`,
-    data: req.body,
-    headers: req.headers,
-  });
-  res.status(response.status).json(response.data);
+  if (req.method === "GET") {
+    const response = await axios.get(url);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "POST") {
+    const response = await axios.post(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method == "PATCH") {
+    const response = await axios.patch(url, req.body);
+    res.status(response.status).json(response.data);
+  } else if (req.method === "DELETE") {
+    const response = await axios.delete(url);
+    res.status(response.status).json(response.data);
+  }
 });
--- api-gateway/middleware/authenticate.js
+++ api-gateway/middleware/authenticate.js
@@ -1,24 +1,22 @@
 const axios = require("axios");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
-const authenticate = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ status: "fail", message: "Unauthorized" });
-    }
-
-    const authURL = `${process.env.AUTH_SERVICE_URL}/verifyToken`;
-    const response = await axios.post(authURL, { token });
-
-    if (response.data.isValid) {
-      req.user = response.data.user;
-      next();
-    } else {
-      res.status(401).json({ status: "fail", message: "Invalid token" });
-    }
-  } catch (error) {
-    res.status(401).json({ status: "fail", message: "Authentication failed" });
+const authenticate = catchAsync(async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ status: "fail", message: "Unauthorized" });
   }
-};
+
+  const authURL = `${process.env.AUTH_SERVICE_URL}/verifyToken`;
+  const response = await axios.post(authURL, { token });
+
+  if (response.data.isValid) {
+    req.user = response.data.user;
+    next();
+  } else {
+    return next(new AppError(`Invalid token`, 401));
+  }
+});
 
 module.exports = authenticate;

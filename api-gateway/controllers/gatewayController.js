bcbbbbb8a4a0bbcba1cc16d50dc5e4dd70a54c82
Hansa Wickramanayake <hansaanuradha93@gmail.com>
2024-12-29 01:43:59 +0530

    cleaned

--- api-gateway/controllers/gatewayController.js
+++ api-gateway/controllers/gatewayController.js
@@ -74,18 +74,10 @@ exports.connectReportService = catchAsync(async (req, res, next) => {
   }
 });
 
-// http://localhost:3001/api/v1/auth/register
-// http://localhost:3001/api/v1/auth/login
-// http://localhost:3001/api/v1/auth/verifyToken
-
 exports.connectAuthService = catchAsync(async (req, res, next) => {
   const url = `${process.env.AUTH_SERVICE_URL}${req.originalUrl}`;
-  console.log(`Auth URL: ${process.env.AUTH_SERVICE_URL}`);
-  console.log(`Original: ${req.originalUrl}`);
-  console.log(`URL: ${url}`);
 
   if (req.method === "POST") {
-    console.log(`POST REQUEST START`);
     const response = await axios.post(url, req.body);
     res.status(response.status).json(response.data);
   }
